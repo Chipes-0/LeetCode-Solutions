92208bc692ca006fc8fab4d980b891bf700e8089
Chipes <carlos1227a@outlook.com>
2026-07-26 22:30:03 -0600

    easy

---
+++ Solutions/1574-maximum-product-of-two-elements-in-an-array/Accepted - 2026-07-26.py
@@ -0,0 +1,10 @@
+from types import List
+import heapq
+
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        heap = []
+        for num in nums:
+            heapq.heappush(heap,  - num)
+        
+        return (-heapq.heappop(heap) - 1) * (-heapq.heappop(heap) - 1)
